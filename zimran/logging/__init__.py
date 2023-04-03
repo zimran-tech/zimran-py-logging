dcccd703baa226b698fbeba894d671ecf8c6fe86
kuanysh-beisembayev <kuanysh.beisembayev@zimran.io>
2023-04-03 11:33:04 +0600

    enable json serialization only on debug

--- zimran/logging/__init__.py
+++ zimran/logging/__init__.py
@@ -6,7 +6,11 @@ from sentry_sdk import init
 
 def setup_logger(debug: bool) -> None:
     logger.remove()
-    logger.add(sys.stdout, level='DEBUG' if debug else 'INFO', serialize=True)
+
+    if debug:
+        logger.add(sys.stdout, level='DEBUG')
+    else:
+        logger.add(sys.stdout, level='INFO', serialize=True)
 
 
 def setup_sentry(dsn: str, environment: str, sample_rate=0.2) -> None:
