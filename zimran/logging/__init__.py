70c651b331a8821f9bc84ee78fdeb46eafcbc601
kuanysh-beisembayev <kuanysh.beisembayev@zimran.io>
2023-04-03 11:55:54 +0600

    split sample rate for multiple environments

--- zimran/logging/__init__.py
+++ zimran/logging/__init__.py
@@ -1,17 +1,3 @@
-import sys
+from .utils import setup_logger, setup_sentry
 
-from loguru import logger
-from sentry_sdk import init
-
-
-def setup_logger(debug: bool) -> None:
-    logger.remove()
-
-    if debug:
-        logger.add(sys.stdout, level='DEBUG')
-    else:
-        logger.add(sys.stdout, level='INFO', serialize=True)
-
-
-def setup_sentry(dsn: str, environment: str, sample_rate=0.2) -> None:
-    init(dsn=dsn, environment=environment, sample_rate=sample_rate)
+__all__ = ['setup_logger', 'setup_sentry']
