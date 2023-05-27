bc86c3d9e8e34b4945cdfe102d05403515e3b3c7
zimtalgat <talgat.abdraimov@zimran.io>
2023-05-27 14:58:18 +0600

    add fastapi specified logging

--- zimran/logging/utils.py
+++ zimran/logging/utils.py
@@ -1,4 +1,5 @@
 import sys
+import uuid
 
 from loguru import logger
 from sentry_sdk import init
@@ -32,3 +33,31 @@ def setup_sentry(dsn: str, environment: str) -> None:
         return
 
     init(dsn=dsn, environment=environment, sample_rate=sample_rate)
+
+
+def setup_fastapi_logger(app, debug: bool):
+    setup_logger(debug=debug)
+
+    @app.middleware('http')
+    def logging_requests(request, call_next):
+        correlation_id = request.headers.get('x-zmrn-correlation-id')
+        if not correlation_id:
+            _set_request_correlation_id(request)
+
+        response = call_next(request)
+        context = {
+            'correlation_id': request.headers.get('x-zmrn-correlation-id'),
+            'method': request.method,
+            'url': str(request.url),
+            'path_params': request.path_params,
+            'query_params': dict(request.query_params),
+        }
+        logger.info('request performed', context=context)
+        return response
+
+
+def _set_request_correlation_id(request):
+    headers = request.headers.mutablecopy()
+    headers.append('x-zmrn-correlation-id', str(uuid.uuid4()))
+    request._headers = headers
+    request.scope.update(headers=request.headers.raw)
