d3ac4f638a5dfad1760e3c0e7d6a5748364eda34
Elkhan Kasymov <elkhankasymov@Elkhans-MacBook-Pro.local>
2024-10-22 17:11:23 +0500

    remove redundent typing annotation

--- zimran/logging/utils.py
+++ zimran/logging/utils.py
@@ -28,7 +28,7 @@ def setup_logger(debug: bool) -> None:
         logger.add(sys.stdout, level='INFO', serialize=True)
 
 
-def setup_sentry(dsn: str, environment: str, **kwargs: dict[str, Any]) -> None:
+def setup_sentry(dsn: str, environment: str, **kwargs) -> None:
     try:
         sample_rate = _get_sample_rate(environment)
     except InvalidEnvironmentError:
